1e0d03a4d9cc9753f7c8102435f13c16326d1fbf
Tawab69 <tawab.eashan.0@gmail.com>
2025-08-21 09:24:25 -0700

    Fix 1.0

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -3,10 +3,6 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
 import { API_URL } from "../constants/api";
-
-// const API_URL = "https://wallet-api-cxqp.onrender.com/api";
-// const API_URL = "http://localhost:5001/api";
-
 export const useTransactions = (userId) => {
   const [transactions, setTransactions] = useState([]);
   const [summary, setSummary] = useState({
